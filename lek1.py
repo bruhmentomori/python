5454ccc93242967bf6475713e10ba12bad5000c4
William Lannerblad <william.lannerblad@learnet.se>
2019-09-03 10:07:50 +0200

    lektion 09-03

--- lek1.py
+++ lek1.py
@@ -1,1 +1,9 @@
 print("hej på er i ee17")
+
+firstName = "William"
+lastName = "Lannerblad"
+#dateOfBirth = 20000518
+age = 19
+teacher = True
+print(firstName , lastName + str(age) + str(teacher))
+print(dateOfBirth)
